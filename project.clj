f657b097b09b9721523defb6bc960b63fcd4758a
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2015-09-15 23:21:26 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reagent-server-rendering "0.1.0-SNAPSHOT"
+(defproject reagent-server-rendering "0.1.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.122"]
                  [reagent "0.5.1"]
@@ -12,8 +12,7 @@
   :plugins [[lein-cljsbuild "1.0.6"]
             [lein-ring "0.9.6"]]
 
-  :ring {:handler reagent-server-rendering.handler/app
-         :uberwar-name "{{name}}.war"}
+  :ring {:handler reagent-server-rendering.handler/app}
 
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target" "test/js"]
 
